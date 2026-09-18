47cd5a5840af088ded310ca17fbcf27c467b0f59
Etherealie <2871645062@qq.com>
2026-09-18 16:23:38 +0800

    版本号提升到 1.1 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mdviewer"
         minSdk = 26          // Android 8.0 起步，覆盖绝大多数手机
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
